89c7c8d0675da875755cec791dbb39c488b7a98e
FBerco <berco.fede@gmail.com>
2016-04-17 17:30:59 -0300

    Agrego clase Banco

---
+++ src/main/java/grupo2/tpAnual/Banco.java
@@ -0,0 +1,14 @@
+package grupo2.tpAnual;
+
+public class Banco {
+	private Disponibilidad _Disponibilidad;
+	
+	public Disponibilidad getDisponibilidad() {
+		return _Disponibilidad;
+	}
+
+	public void setDisponibilidad(Disponibilidad dis) {
+		this._Disponibilidad = dis;
+	}
+	
+}
