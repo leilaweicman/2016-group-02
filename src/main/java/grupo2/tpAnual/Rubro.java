ae2675f3eaedcf1e47de3793e047e5c442574846
FBerco <berco.fede@gmail.com>
2016-04-17 17:29:21 -0300

    Agrego clase rubro

---
+++ src/main/java/grupo2/tpAnual/Rubro.java
@@ -0,0 +1,22 @@
+package grupo2.tpAnual;
+
+public class Rubro {
+	private int _RadioCercania;
+	private Disponibilidad _Disponibilidad;
+	
+	public Disponibilidad getDisponibilidad() {
+		return _Disponibilidad;
+	}
+
+	public void setDisponibilidad(Disponibilidad dis) {
+		this._Disponibilidad = dis;
+	}
+	
+	public int getRadioCercania() {
+		return _RadioCercania;
+	}
+
+	public void setRadioCercania(int Radio) {
+		this._RadioCercania = Radio;
+	}
+}
