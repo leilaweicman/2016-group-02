61c6385a0dc378f590eb7ac15429857bc8efba26
FBerco <berco.fede@gmail.com>
2016-04-17 17:04:44 -0300

    Agrego getters y setters

--- src/main/java/grupo2/tpAnual/Direccion.java
+++ src/main/java/grupo2/tpAnual/Direccion.java
@@ -3,4 +3,19 @@ package grupo2.tpAnual;
 public class Direccion {
 	public String Calle;
 	public int Altura;
+	
+	public String getCalle() {
+		return Calle;
+	}
+
+	public void setCalle(String cl) {
+		this.Calle = cl;
+	}
+	public int getAltura() {
+		return Altura;
+	}
+
+	public void setPrecioBase(int alt) {
+		this.Altura = alt;
+	}
 }
