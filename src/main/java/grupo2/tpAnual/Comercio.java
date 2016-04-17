224d2373c3a15583219b767782e834c6cbc052cf
FBerco <berco.fede@gmail.com>
2016-04-17 17:30:28 -0300

    Agrego clase Comercio

---
+++ src/main/java/grupo2/tpAnual/Comercio.java
@@ -0,0 +1,13 @@
+package grupo2.tpAnual;
+
+public class Comercio {
+	private Rubro _Rubro;
+	
+	public Rubro getRubro() {
+		return _Rubro;
+	}
+
+	public void setRubro(Rubro rub) {
+		this._Rubro = rub;
+	}
+}
