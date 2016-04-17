5dc03aeab3030055aeb86e2324698efa217a6dd8
FBerco <berco.fede@gmail.com>
2016-04-17 17:10:49 -0300

    Agrego getters y setters

---
+++ src/main/java/grupo2/tpAnual/POI.java
@@ -0,0 +1,15 @@
+package grupo2.tpAnual;
+
+
+public class POI {
+	public Direccion Direccion;
+	//public Point Point; 
+	
+	public Direccion getDireccion() {
+		return Direccion;
+	}
+
+	public void setDireccion(Direccion dir) {
+		this.Direccion = dir;
+	}
+}
