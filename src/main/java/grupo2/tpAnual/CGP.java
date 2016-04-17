aa720eee30c82ab07f5d6333a336a738a5a65f50
FBerco <berco.fede@gmail.com>
2016-04-17 17:25:06 -0300

    Agrego CGP

---
+++ src/main/java/grupo2/tpAnual/CGP.java
@@ -0,0 +1,16 @@
+package grupo2.tpAnual;
+import java.util.List;
+
+
+public class CGP {
+	private List<Servicio> _Servicios;
+	
+	public List<Servicio> getServicios(){
+		return _Servicios;
+	}
+	public void setServicio(List<Servicio> ser) {
+		this._Servicios = ser;
+	}
+	
+	
+}
