4642381ac3dbc684f06a1976276ec52f098f242a
FBerco <berco.fede@gmail.com>
2016-04-17 17:27:10 -0300

    Agrego clase Servicio

---
+++ src/main/java/grupo2/tpAnual/Servicio.java
@@ -0,0 +1,24 @@
+package grupo2.tpAnual;
+
+public class Servicio {
+	private String _Nombre;
+	private Disponibilidad _Disponibilidad;
+	
+	public String _Nombre() {
+		return _Nombre;
+	}
+
+	public void setNombre(String Nom) {
+		this._Nombre = Nom;
+	}
+	
+	public Disponibilidad getDisponibilidad() {
+		return _Disponibilidad;
+	}
+
+	public void setDisponibilidad(Disponibilidad dis) {
+		this._Disponibilidad = dis;
+	}
+	
+	
+}
