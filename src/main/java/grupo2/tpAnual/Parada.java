a18dc2689d3a47e26df1aeeacf6da7075a2d61e9
FBerco <berco.fede@gmail.com>
2016-04-17 21:35:25 -0300

    Agrego linea de colectivo

--- src/main/java/grupo2/tpAnual/Parada.java
+++ src/main/java/grupo2/tpAnual/Parada.java
@@ -3,6 +3,16 @@ package grupo2.tpAnual;
 import java.util.Date;
 
 public class Parada extends POI{
+	private int _Linea;
+	
+	public int getLinea() {
+		return _Linea;
+	}
+
+	public void setLinea(int linea) {
+		this._Linea = linea;
+	}
+	
 	public boolean CalcularDisponibilidad(Date fecha){
 		return true;
 	}
