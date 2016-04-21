365db916be9cc34d482d88a3e75b64375764fa3e
rabinovichm <michu.rabinovich@hotmail.com>
2016-04-21 01:23:47 -0300

    estaDisponible banco test

--- src/test/java/grupo2/tpAnual/BancoTest.java
+++ src/test/java/grupo2/tpAnual/BancoTest.java
@@ -1,14 +1,28 @@
 package grupo2.tpAnual;
-
-import static org.junit.Assert.*;
-
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class BancoTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+public class BancoTest {	
+	
+	 private Banco banco;
+	 private DateTime momento;
+	 @Before
+	 public void init(){
+	  banco= new Banco();
+	 }
+	 
+	 @Test
+	 public void estaDisponibleLunesALas11() {
+		 momento = new DateTime("2016-04-25T11:00:00");
+		 Assert.assertTrue(banco.estaDisponible(momento)); 
+	 }
+	 
+	 @Test
+	 public void noEstaDisponibleViernesALas16() {
+		 momento = new DateTime("2016-04-22T16:00:00");
+		 Assert.assertFalse(banco.estaDisponible(momento)); 
+	 }
 
 }
