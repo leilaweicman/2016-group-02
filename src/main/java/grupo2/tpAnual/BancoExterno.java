351f2d41ddbc11f65982f7530b5994862087316d
FBerco <berco.fede@gmail.com>
2016-05-07 16:56:59 -0300

    Agrego getters y setters

--- src/main/java/grupo2/tpAnual/BancoExterno.java
+++ src/main/java/grupo2/tpAnual/BancoExterno.java
@@ -6,12 +6,12 @@ import org.joda.time.DateTime;
 import org.uqbar.geodds.Point;
 
 public class BancoExterno extends POI {
-	public String banco;
-	public double x;
-	public double y;
-	public String sucursal;
-	public String gerente;
-	public List<String> servicios;
+	private String banco;
+	private double x;
+	private double y;
+	private String sucursal;
+	private String gerente;
+	private List<String> servicios;
 	@Override
 	boolean estaDisponible(DateTime momento, String nombreServicio) {
 		// TODO Auto-generated method stub
@@ -21,5 +21,41 @@ public class BancoExterno extends POI {
 	boolean estaCerca(Point coordenadaDeseada) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	public String getBanco() {
+		return banco;
+	}
+	public void setBanco(String banco) {
+		this.banco = banco;
+	}
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	public String getSucursal() {
+		return sucursal;
+	}
+	public void setSucursal(String sucursal) {
+		this.sucursal = sucursal;
+	}
+	public List<String> getServicios() {
+		return servicios;
+	}
+	public void setServicios(List<String> servicios) {
+		this.servicios = servicios;
+	}
+	public String getGerente() {
+		return gerente;
+	}
+	public void setGerente(String gerente) {
+		this.gerente = gerente;
 	}	
 }
