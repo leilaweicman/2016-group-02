aa106ca3b00c0082b7ba736f9d3ef986ca379333
FBerco <berco.fede@gmail.com>
2016-05-07 17:03:14 -0300

    Add CentroDTO
    Falta completar la lista de serviciosDTO y hacer getters y setters. No
    lo hago porque quiero tener mas información de lo que hay que hacer

---
+++ src/main/java/grupo2/tpAnual/CentroDTO.java
@@ -0,0 +1,12 @@
+package grupo2.tpAnual;
+
+import java.util.List;
+
+public class CentroDTO {
+	private int numeroComuna;
+	private String zonas;
+	private String nombreDirector;
+	private String domicilio;
+	private String telefono;
+	//private List<servicioDTO> serviciosDTO;
+}
