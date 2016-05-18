154b53a990e17c98670b1e603afdbe9c88245616
Leila Weicman <leilaweic@hotmail.com>
2016-05-18 00:46:09 -0300

    cambios en clase disponibilidad

--- src/main/java/grupo2/tpAnual/Disponibilidad.java
+++ src/main/java/grupo2/tpAnual/Disponibilidad.java
@@ -10,7 +10,12 @@ public class Disponibilidad {
 	
 	private List<Rango> rangoDisponibilidad = new ArrayList<Rango>();
 	
-	public boolean estaDisponible(DateTime momento, String nombre) {
+	// constructor
+	public Disponibilidad(List<Rango> rango) {
+		this.rangoDisponibilidad = rango;
+	}
+		
+	public boolean estaDisponible(DateTime momento) {
 		int dia = momento.getDayOfWeek();
 		int hora = momento.getHourOfDay();
 		int minutos = momento.getMinuteOfHour();
@@ -18,13 +23,9 @@ public class Disponibilidad {
 		
 		LocalTime horaCompleta = LocalTime.of(hora, minutos, segundos);
 
-		//Time horaCompleta = new Time(hora, minutos, segundos);
 		boolean disponible = false;
 		
-		// USAR MENSAJES DE COLECCIONES
-		// col.stream().anyMatch(elem -> cond );
-		
-		disponible = rangoDisponibilidad.stream().anyMatch(rango -> rango.estaDisponible(dia, horaCompleta));
+		disponible = this.rangoDisponibilidad.stream().anyMatch(rango -> rango.estaDisponible(dia, horaCompleta));
 		
 		/*for (Rango rango : rangoDisponibilidad) {
 			
