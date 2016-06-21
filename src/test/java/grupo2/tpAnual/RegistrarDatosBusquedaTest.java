9caafda1de2df9473951ddfa51d82943a3068969
Ailen Muñoz <ailenmunozz@gmail.com>
2016-06-21 20:19:55 -0300

    arreglo test de registrarDatosBusqueda

--- src/test/java/grupo2/tpAnual/RegistrarDatosBusquedaTest.java
+++ src/test/java/grupo2/tpAnual/RegistrarDatosBusquedaTest.java
@@ -7,14 +7,14 @@ import org.junit.Test;
 
 import grupo2.tpAnual.Observers.NotificarDatosBusqueda;
 
-public class RegistrarBusquedaTest {
-	NotificarDatosBusqueda observer = new NotificarDatosBusqueda();
+public class RegistrarDatosBusquedaTest {
+	RegistrarDatosBusqueda registro = new RegistrarDatosBusqueda();
 	DatosDeBusqueda datoBuscado = new DatosDeBusqueda("lasHeras","libros", 18, 10, 15, new LocalDate());
 
 	@Test
-	public void notificarBusquedaTest() {
-	/*	this.observer.notificarBusqueda(datoBuscado);
-		Assert.assertEquals(this.observer.getRegistroBusqueda().size(), 1);*/
+	public void agregarDatosBusquedaTest() {
+	this.registro.agregarDatosBusqueda(datoBuscado);
+	Assert.assertEquals(this.registro.consultarDatos().size(), 1);
 
 	}
 }
