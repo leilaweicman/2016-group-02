6b04d410b40b5119d21677db3d4c8bee43ea232c
Ailen Muñoz <ailenmunozz@gmail.com>
2016-06-21 23:08:19 -0300

    agrego mas tests para probar la busqueda desde origenesDeDatosPOI

--- src/test/java/grupo2/tpAnual/OrigenesDeDatosPOIsTest.java
+++ src/test/java/grupo2/tpAnual/OrigenesDeDatosPOIsTest.java
@@ -13,31 +13,75 @@ import grupo2.tpAnual.OrigenesDeDatos.OrigenesDeDatosPOIs;
 public class OrigenesDeDatosPOIsTest {
 	private OrigenesDeDatosPOIs origenesPOI;
 	private Comercio kosiuko;
+	private Banco bancoPiano;
+	private CGP cgp;
+	private Servicio jubilacion;
 	private Rango unRango;
 	private List<Rango> listaRangos;
+	private List<Servicio> servicios;
 
 	@Before
 	public void init() {
+
 		unRango = new Rango();
 		unRango.setDia(1);
 		unRango.setHoraDesde(LocalTime.of(9, 0, 0));
 		unRango.setHoraHasta(LocalTime.of(18, 0, 0));
 		listaRangos = Arrays.asList(unRango);
+
 		this.kosiuko = new Comercio(listaRangos);
+		this.bancoPiano = new Banco();
+		this.cgp = new CGP();
 
 		this.origenesPOI = new OrigenesDeDatosPOIs();
 
 	}
 
 	@Test
-	public void testCrearPoiAcierto() {
+	public void crearPoiTest() {
 		this.origenesPOI.agregarPOI(kosiuko);
 		Assert.assertEquals(this.origenesPOI.getPOIs().size(), 1);
 	}
 
-	/*
-	 * @Test public void testBusquedaPorServicio() {
-	 * //Assert.assertTrue(rentas.VerificarPorTexto("Jubilados")); }
-	 */
+	@Test
+	public void darDeBajaPoiTest() {
+		this.origenesPOI.agregarPOI(kosiuko);
+		this.origenesPOI.agregarPOI(bancoPiano);
+		this.origenesPOI.agregarPOI(cgp);
+		this.origenesPOI.darDeBajaPOI(kosiuko);
+		Assert.assertEquals(this.origenesPOI.getPOIs().size(), 2);
+	}
 
+	@Test
+	public void busquedaTestAssert() {
+
+		this.kosiuko.addPalabraClave("muchaGente");
+		this.bancoPiano.addPalabraClave("muchaGente");
+		this.cgp.addPalabraClave("muchaGente");
+
+		this.origenesPOI.agregarPOI(cgp);
+		this.origenesPOI.agregarPOI(kosiuko);
+		this.origenesPOI.agregarPOI(bancoPiano);
+
+		Assert.assertEquals(this.origenesPOI.busqueda("muchaGente").size(), 3);
+	}
+
+	@Test
+	public void busquedaTestFail() {
+		this.jubilacion = new Servicio(listaRangos);
+		this.jubilacion.setNombre("jubilacionesPami");
+		servicios = Arrays.asList(jubilacion);
+		this.cgp.setServicios(servicios);
+		
+		this.cgp.addPalabraClave("muchaGente");
+		this.kosiuko.addPalabraClave("muchaGente");
+		this.bancoPiano.addPalabraClave("muchaGente");
+		
+
+		this.origenesPOI.agregarPOI(cgp);
+		this.origenesPOI.agregarPOI(kosiuko);
+		this.origenesPOI.agregarPOI(bancoPiano);
+
+		Assert.assertEquals(this.origenesPOI.busqueda("hola").size(), 0);
+	}
 }
