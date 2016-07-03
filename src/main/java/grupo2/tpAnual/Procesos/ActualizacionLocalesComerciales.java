01428a0ca6fe05a2d1c8f2eebdf00a3e7e685113
FBerco <berco.fede@gmail.com>
2016-07-03 20:47:48 -0300

    Lo obtengo desde un file

--- src/main/java/grupo2/tpAnual/Procesos/ActualizacionLocalesComerciales.java
+++ src/main/java/grupo2/tpAnual/Procesos/ActualizacionLocalesComerciales.java
@@ -1,6 +1,9 @@
 package grupo2.tpAnual.Procesos;
 
 
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,16 +26,28 @@ public class ActualizacionLocalesComerciales extends Proceso {
 
 	@Override
 	public void ejecutarProceso(LogEjecucionProcesos log) {
-		WebResource.Builder builder = this.client.resource(origen).accept(MediaType.TEXT_PLAIN);
-		String response = builder.get(String.class);
-		String[] componente = response.split(";");
-		//Obtengo unicamente los que son comercio de la busqueda de pois
-		List<Comercio> comercios = this.origenesDeDatos.getPOIs().stream().filter(x-> x instanceof Comercio)
-									.map(p -> (Comercio) p).collect(Collectors.toList());
-		for(Comercio com : comercios){
-			if(com.getNombre() == componente[0]){
-				com.setPalabrasClaves(Arrays.asList(componente[1].split(" ")));
-			}
-		}		
+		//WebResource.Builder builder = this.client.resource(origen).accept(MediaType.TEXT_PLAIN);
+		//String response = builder.get(String.class);
+		
+		try {
+			String response = getFile();		
+			String[] componente = response.split(";");
+			//Obtengo unicamente los que son comercio de la busqueda de pois
+			List<Comercio> comercios = this.origenesDeDatos.getPOIs().stream().filter(x-> x instanceof Comercio)
+										.map(p -> (Comercio) p).collect(Collectors.toList());
+			for(Comercio com : comercios){
+				if(com.getNombre() == componente[0]){
+					com.setPalabrasClaves(Arrays.asList(componente[1].split(" ")));
+				}
+			}	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private String getFile() throws IOException{		
+		File file = new File(origen);
+		return FileUtils.readFileToString(file);
 	}
 }
