f14547db73b80e8dfc7dcd28e970fc8aa97eead6
FBerco <berco.fede@gmail.com>
2016-07-03 22:57:20 -0300

    Test de ActualizacionLocalesComerciales

--- src/main/java/grupo2/tpAnual/Procesos/ActualizacionLocalesComerciales.java
+++ src/main/java/grupo2/tpAnual/Procesos/ActualizacionLocalesComerciales.java
@@ -7,21 +7,18 @@ import org.apache.commons.io.FileUtils;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.ws.rs.core.MediaType;
 import org.joda.time.LocalDate;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
 import grupo2.tpAnual.Comercio;
 import grupo2.tpAnual.OrigenesDeDatos.OrigenesDeDatosPOIs;
 import grupo2.tpAnual.Procesos.ManejoDeErroresProcesos.AccionEnCasoDeFallo;
 
 public class ActualizacionLocalesComerciales extends Proceso {
-	private Client client;
-	private String origen;
+	//HAY QUE VER DE DONDE OBTENGO EL ORIGEN
+	private String origen = "C:\\Users\\pc hogar.pc\\Desktop\\prueba.txt";
+	
 	public ActualizacionLocalesComerciales(int hora, LocalDate fecha, AccionEnCasoDeFallo configuracion, OrigenesDeDatosPOIs origenesDeDatos) {
 		super(hora, fecha, configuracion, origenesDeDatos);
 		// TODO Auto-generated constructor stub
-		client = Client.create();
 	}
 
 	@Override
@@ -30,13 +27,13 @@ public class ActualizacionLocalesComerciales extends Proceso {
 		//String response = builder.get(String.class);
 		
 		try {
-			String response = getFile();		
+			String response = getFile(origen);		
 			String[] componente = response.split(";");
 			//Obtengo unicamente los que son comercio de la busqueda de pois
 			List<Comercio> comercios = this.origenesDeDatos.getPOIs().stream().filter(x-> x instanceof Comercio)
 										.map(p -> (Comercio) p).collect(Collectors.toList());
 			for(Comercio com : comercios){
-				if(com.getNombre() == componente[0]){
+				if(com.getNombre().equals(componente[0])){
 					com.setPalabrasClaves(Arrays.asList(componente[1].split(" ")));
 				}
 			}	
@@ -46,8 +43,8 @@ public class ActualizacionLocalesComerciales extends Proceso {
 		}
 	}
 	
-	private String getFile() throws IOException{		
-		File file = new File(origen);
+	public String getFile(String ruta) throws IOException{		
+		File file = new File(ruta);
 		return FileUtils.readFileToString(file);
 	}
 }
--- src/test/java/grupo2/tpAnual/Procesos/ActualizacionLocalesComercialesTest.java
+++ src/test/java/grupo2/tpAnual/Procesos/ActualizacionLocalesComercialesTest.java
@@ -1,5 +1,61 @@
 package grupo2.tpAnual.Procesos;
 
-public class ActualizacionLocalesComercialesTest {
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+import grupo2.tpAnual.Comercio;
+import grupo2.tpAnual.Rango;
+import grupo2.tpAnual.OrigenesDeDatos.OrigenesDeDatosPOIs;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+
+public class ActualizacionLocalesComercialesTest {
+	private OrigenesDeDatosPOIs origenesDeDatos;
+	private LogEjecucionProcesos log;
+	private ActualizacionLocalesComerciales proceso;
+	@Before
+	public void init() {
+		origenesDeDatos = new OrigenesDeDatosPOIs();
+		Rango unRango = new Rango();
+		Rango otroRango = new Rango();
+		Rango rango = new Rango();
+		
+		unRango.setDia(1);
+		unRango.setHoraDesde(LocalTime.of(9, 0, 0));
+		unRango.setHoraHasta(LocalTime.of(18, 0, 0));
+
+		otroRango.setDia(3);
+		otroRango.setHoraDesde(LocalTime.of(9, 0, 0));
+		otroRango.setHoraHasta(LocalTime.of(13, 0, 0));
+
+		rango.setDia(3);
+		rango.setHoraDesde(LocalTime.of(15, 0, 0));
+		rango.setHoraHasta(LocalTime.of(18, 30, 0));
+
+		List<Rango>  listaRangos = Arrays.asList(unRango, otroRango, rango);
+
+		Comercio comercio = new Comercio(listaRangos, "Carrousel");
+		origenesDeDatos.agregarPOI(comercio);
+		log = new LogEjecucionProcesos();
+		proceso = new ActualizacionLocalesComerciales(14, new LocalDate(),null,origenesDeDatos);
+	}
+	
+	//@Test
+	//No es un test, simplemente lo corro para ver si lee bien el archivo
+	//public void obtenerStringDeArchivo() throws Exception {
+	//	System.out.println(proceso.getFile("C:\\Users\\pc hogar.pc\\Desktop\\prueba.txt")); 
+	//}
+	
+	@Test
+	public void ejecutarTest() {
+		proceso.ejecutarProceso(log);
+		Comercio com = (Comercio)origenesDeDatos.getPOIs().iterator().next() ;
+		Assert.assertEquals(com.getPalabraClave().size(), 4);
+	}
 }
