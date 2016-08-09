378fb6afb7fa59a562f830fb9edd7655db053eb1
Ailen Muñoz <ailenmunozz@gmail.com>
2016-08-09 16:10:14 -0300

    borro test que no aportaba mucho porque otro verificaba lo mismo. Issue 29

--- src/test/java/grupo2/tpAnual/NotificarBusquedaTest.java
+++ src/test/java/grupo2/tpAnual/NotificarBusquedaTest.java
@@ -8,27 +8,11 @@ import grupo2.tpAnual.Observers.NotificarDatosBusqueda;
 
 public class NotificarBusquedaTest {
 	DatosDeBusqueda datoBuscado = new DatosDeBusqueda("corrientes", "peras", 10, 15, new LocalDate());
-	DatosDeBusqueda datoBuscado1 = new DatosDeBusqueda("callao", "bananas", 10, 15, new LocalDate());
-	DatosDeBusqueda datoBuscado2 = new DatosDeBusqueda("lasHeras", "ropa", 10, 15, new LocalDate());
-	DatosDeBusqueda datoBuscado3 = new DatosDeBusqueda("flores", "veterinaria", 10, 15, new LocalDate());
-	DatosDeBusqueda datoBuscado4 = new DatosDeBusqueda("caballito", "hospital", 10, 15, new LocalDate());
-
 	NotificarDatosBusqueda observer = new NotificarDatosBusqueda();
 
 	@Test
 	public void notificarDatosDeBusquedaTest() {
 		this.observer.notificarBusqueda(datoBuscado);
-		Assert.assertEquals(this.observer.getRegister().consultarDatos().size(), 1);
-	}
-
-	@Test
-	public void notificarDatosDeBusquedaMuchosDatosTest() {
-		this.observer.notificarBusqueda(datoBuscado);
-		this.observer.notificarBusqueda(datoBuscado1);
-		this.observer.notificarBusqueda(datoBuscado2);
-		this.observer.notificarBusqueda(datoBuscado3);
-		this.observer.notificarBusqueda(datoBuscado4);
-		this.observer.notificarBusqueda(datoBuscado);
-		Assert.assertEquals(this.observer.getRegister().consultarDatos().size(), 6);
+		Assert.assertEquals(this.observer.getRegister().consultarDatos().contains(datoBuscado), 1);
 	}
 }
