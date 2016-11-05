5a3027477d610401d3218a76977c65938f76ff47
FBerco <berco.fede@gmail.com>
2016-11-04 21:06:34 -0300

    Agrego vista y controller de Admin Pois

---
+++ src/main/java/grupo2/tpAnual/Web/Controller/AdministracionPoisController.java
@@ -0,0 +1,11 @@
+package grupo2.tpAnual.Web.Controller;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+public class AdministracionPoisController {
+	public static ModelAndView get(Request req, Response res) {
+		return new ModelAndView(null, "resultadoBusquedaTerminal.hbs");
+	}
+}
