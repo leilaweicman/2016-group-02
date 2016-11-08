c479ec4a70c09baa72037c61169325192a9626af
FBerco <berco.fede@gmail.com>
2016-11-07 23:31:16 -0300

    Paso a static

--- src/main/java/grupo2/tpAnual/Web/Controller/AdministracionPoisController.java
+++ src/main/java/grupo2/tpAnual/Web/Controller/AdministracionPoisController.java
@@ -1,11 +1,15 @@
 package grupo2.tpAnual.Web.Controller;
 
+import java.util.ArrayList;
+import grupo2.tpAnual.Web.Models.Mapa;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
 public class AdministracionPoisController {
 	public static ModelAndView get(Request req, Response res) {
-		return new ModelAndView(null, "administracionPois.hbs");
+		grupo2.tpAnual.Mapa mapa = Mapa.get();
+		mapa.busquedaRealizadaPorElUsuario("");
+		return new ModelAndView(new ArrayList<>(), "administracionPois.hbs");
 	}
 }
--- src/main/java/grupo2/tpAnual/Web/Models/Mapa.java
+++ src/main/java/grupo2/tpAnual/Web/Models/Mapa.java
@@ -3,7 +3,7 @@ package grupo2.tpAnual.Web.Models;
 public class Mapa {
 	private static grupo2.tpAnual.Mapa instance;
 
-	public grupo2.tpAnual.Mapa get() {
+	public static grupo2.tpAnual.Mapa get() {
 		if (instance == null) {
 			//TODO: Hay que pasarle los parametros correspondientes a mapa
 			instance = new grupo2.tpAnual.Mapa(null, null);
