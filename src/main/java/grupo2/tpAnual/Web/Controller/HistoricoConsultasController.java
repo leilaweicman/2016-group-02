3d3f9c0b898ab0c9f33da9bc6e0e618c22fbebc4
mtisot <mtisot@gmail.com>
2016-11-09 17:00:47 -0300

    Cambio la vista de historico consultas y empiezo el controller

--- src/main/java/grupo2/tpAnual/Web/Controller/HistoricoConsultasController.java
+++ src/main/java/grupo2/tpAnual/Web/Controller/HistoricoConsultasController.java
@@ -1,11 +1,50 @@
 package grupo2.tpAnual.Web.Controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import grupo2.tpAnual.Repositorios.DatosBusquedaRepositoryMongoDB;
+import grupo2.tpAnual.Repositorios.DatosDeBusqueda;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
 public class HistoricoConsultasController {
-	public static ModelAndView get(Request req, Response res) {
-		return new ModelAndView(null, "historicoConsultas.hbs");
+	
+	public static ModelAndView listar(Request req, Response res) {
+		Map<String, List<DatosDeBusqueda>> model = new HashMap<>();
+		List<DatosDeBusqueda> datosDeBusqueda = DatosBusquedaRepositoryMongoDB.instancia.consultarDatos();
+		
+		model.put("datosDebusqueda", datosDeBusqueda);
+		
+		return new ModelAndView(model, "historicoConsultas.hbs");
+		
 	}
+	
+	public static ModelAndView listarPorTerminal(Request req, Response res) {
+		Map<String, List<DatosDeBusqueda>> model = new HashMap<>();
+		
+		String terminal= req.params("terminal");
+		
+		List<DatosDeBusqueda> datosDeBusqueda = DatosBusquedaRepositoryMongoDB.instancia.obtenerPorNombre(terminal);
+		
+		model.put("datosDebusqueda", datosDeBusqueda);
+		
+		return new ModelAndView(model, "historicoConsultas.hbs");
+		
+	}
+	
+	/*public static ModelAndView listarPorFecha(Request req, Response res) {
+		Map<String, List<DatosDeBusqueda>> model = new HashMap<>();
+		
+		String fecha= req.params("fecha");
+		
+		List<DatosDeBusqueda> datosDeBusqueda = DatosBusquedaRepositoryMongoDB.instancia.(fecha);
+		
+		model.put("datosDebusqueda", datosDeBusqueda);
+		
+		return new ModelAndView(model, "historicoConsultas.hbs");
+		
+	}*/
 }
