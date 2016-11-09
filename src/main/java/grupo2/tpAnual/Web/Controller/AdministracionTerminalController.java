3d67f1c690921289a3349b36c22092eea4d6a6f2
rabinovichm <michu.rabinovich@hotmail.com>
2016-11-09 19:55:10 -0300

    arreglo travis posta

--- src/main/java/grupo2/tpAnual/Web/Controller/AdministracionTerminalController.java
+++ src/main/java/grupo2/tpAnual/Web/Controller/AdministracionTerminalController.java
@@ -10,7 +10,6 @@ import org.uqbar.geodds.Point;
 import grupo2.tpAnual.AccesoriosPois.Comuna;
 import grupo2.tpAnual.Pois.POI;
 import grupo2.tpAnual.Repositorios.MemoryUserRepository;
-import grupo2.tpAnual.Repositorios.POIsRepository;
 import grupo2.tpAnual.Repositorios.Usuario;
 import spark.ModelAndView;
 import spark.Request;
@@ -55,26 +54,4 @@ public class AdministracionTerminalController {
 		
 	}
 	
-	public static ModelAndView listar(Request req, Response res) {
-		Map<String, List<POI>> model = new HashMap<>();
-		List<POI> pois = POIsRepository.instancia.listar();
-		
-		model.put("pois", pois);
-		return new ModelAndView(model, "administracionTerminal.hbs");
-		/*Map<String, List<Mapa>> model = new HashMap<>();
-		List<Mapa> mapas = RepositorioProyectos.instancia.listar();
-		
-		model.put("proyectos", proyectos);
-		return new ModelAndView(model, "proyectos/index.hbs");
-
-		
-		Map<String, List<POI>> model = new HashMap<>();
-		String palabraBuscada = req.params("palabraBuscada");
-		Mapa mapa = SingletonMapa.get();
-		List<POI> resultadoBusqueda = new ArrayList<>();
-		//resultadoBusqueda = mapa.busquedaRealizadaPorElUsuario(palabraBuscada);
-		resultadoBusqueda = Arrays.asList(new Banco("Santander", Point.and(123.4, 223.4)), new Comercio("Zara",Point.and(87.777, 55.435),null), new CGP("Comuna 9", Point.and(255.4, 333.2)));
-		model.put("pois",resultadoBusqueda);*/
-		//return new ModelAndView(null, "listarTerminales.hbs");
-	}
 }
