45b352963e4bf2a35ce70e4ceb1848f9d57e8d1b
Leila Weicman <leilaweic@hotmail.com>
2017-01-06 00:25:58 -0300

    unifico el listar

--- src/main/java/grupo2/tpAnual/Web/Controller/HistoricoConsultasController.java
+++ src/main/java/grupo2/tpAnual/Web/Controller/HistoricoConsultasController.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import grupo2.tpAnual.Mapa;
 import grupo2.tpAnual.Repositorios.DatosBusquedaRepositoryMongoDB;
 import grupo2.tpAnual.Repositorios.DatosDeBusqueda;
+import grupo2.tpAnual.Repositorios.DatosDeBusquedaRepository;
+import grupo2.tpAnual.Web.Models.SingletonDatosBusquedaRepository;
 import grupo2.tpAnual.Web.Models.SingletonMapa;
 import spark.ModelAndView;
 import spark.Request;
@@ -25,22 +27,21 @@ public class HistoricoConsultasController {
 	}
 	
 	public static ModelAndView listar(Request req, Response res) {
+		
+		//TODO Recibir los parametros y fijarme cuales no estan vacios para ver por cuales filtrar
+		
 		Map<String, List<DatosDeBusqueda>> model = new HashMap<>();
 		
 		String terminal= req.queryParams("terminal");
 		
-		//Mapa mapa = SingletonMapa.get();
+		DatosDeBusquedaRepository repository = SingletonDatosBusquedaRepository.get();
 		
-		/*Cosas para hacer:
-		 * 1. Hay que crear una instancia de DatosDeBusquedaRepository que sea unica (singleton), para que haya datos precargados cuando se realice la busqueda 
-		 * 2. filtrar por terminal, fecha y cantidad de pois -> habria que crear un metodo que filtre por las 3 cosas
-		 */		
-		
-		List<DatosDeBusqueda> datosDeBusqueda = DatosBusquedaRepositoryMongoDB.instancia.obtenerPorNombre(terminal);
+		//List<DatosDeBusqueda> datosDeBusqueda = DatosBusquedaRepositoryMongoDB.instancia.obtenerPorNombre(terminal);
+		List<DatosDeBusqueda> datosDeBusqueda = repository.obtenerPorNombre(terminal);
 		
 		model.put("datosDebusqueda", datosDeBusqueda);
 		
-		return new ModelAndView(model, "historicoConsultas/listarPorTerminal.hbs");
+		return new ModelAndView(model, "historicoConsultas/listar.hbs");
 		
 	}
 	
